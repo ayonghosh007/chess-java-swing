3324db8934cea16b70aebc22100ff472c373a078
ayonghosh007 <ghosh.ayon200512@gmail.com>
2026-05-31 01:24:35 +0530

    Frame and Constants

---
+++ Chess Java Swing/src/window/Frame.java
@@ -0,0 +1,14 @@
+package window;
+
+import javax.swing.JFrame;
+
+import main.Constants;
+
+public class Frame extends JFrame implements Constants {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7929949333044430064L;
+
+}
